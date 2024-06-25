162508e4ec982ddfb5c24ea38bce177dbec262ed
mariaclr21 <marialyrio@ucl.br>
2024-06-25 14:00:35 -0300

    Update doce.cs

--- doce.cs
+++ doce.cs
@@ -1,1 +1,46 @@
+class Doce
+{
+    private string NomeDoce;
+    private decimal PrecoDoce;
+    private int QuantidadeDoce;
 
+    public Doce(string NomeDoce, decimal PrecoDoce)
+    {
+        this.NomeDoce = NomeDoce;
+        this.PrecoDoce = PrecoDoce;
+        QuantidadeDoce = 0;
+    }
+
+    public Doce(string NomeDoce, decimal PrecoDoce, int Quantidade)
+    {
+        this.NomeDoce = NomeDoce;
+        this.PrecoDoce = PrecoDoce;
+        this.QuantidadeDoce = QuantidadeDoce;
+    }
+    public string getNomeDoce()
+    {
+        return NomeDoce;
+    }
+    public void setNomeDoce(string nd)
+    {
+        NomeDoce = nd;
+    }
+
+    public decimal getPrecoDoce()
+    {
+        return PrecoDoce;
+    }
+    public void setPrecoDoce(decimal pd)
+    {
+        PrecoDoce = pd;
+    }
+
+    public int getQuantidadeDoce()
+    {
+        return QuantidadeDoce;
+    }
+    public void setQuantidadeDoce(int qd)
+    {
+        QuantidadeDoce = qd;
+    }
+}
